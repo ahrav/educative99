dfc7440323a81059c9a3632be3c2a90637ac9f8c
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-10-08 13:27:05 -0700

    remove nth item linked list

--- two_pointers/twopointers.go
+++ two_pointers/twopointers.go
@@ -17,3 +17,98 @@ func isPalindrome(s string) bool {
 
 	return true
 }
+
+// ListNode is a node in a linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// InitListNode initializes a linked list from a single integer.
+func InitListNode(val int) *ListNode {
+	return &ListNode{Val: val, Next: nil}
+}
+
+// LinkedList is a linked list.
+type LinkedList struct {
+	Head *ListNode
+}
+
+// InsertAtHead inserts a node at the head of a linked list.
+func (ll *LinkedList) InsertAtHead(node *ListNode) {
+	if ll == nil {
+		return
+	}
+
+	node.Next = ll.Head
+	ll.Head = node
+}
+
+// ConstructLinkedListFromNode constructs a linked list from a node.
+func ConstructLinkedListFromNode(node *ListNode) *LinkedList {
+	if node == nil {
+		return nil
+	}
+
+	return &LinkedList{Head: node}
+}
+
+// InitLinkedList initializes a linked list from a slice of integers.
+func InitLinkedList(vals []int) *LinkedList {
+	if len(vals) == 0 {
+		return nil
+	}
+
+	head := InitListNode(vals[0])
+	curr := head
+	for i := 1; i < len(vals); i++ {
+		curr.Next = InitListNode(vals[i])
+		curr = curr.Next
+	}
+
+	return &LinkedList{Head: head}
+}
+
+// Display displays a linked list.
+func (ll *LinkedList) Display() []int {
+	if ll == nil {
+		return nil
+	}
+
+	var vals []int
+	curr := ll.Head
+	for curr != nil {
+		vals = append(vals, curr.Val)
+		curr = curr.Next
+	}
+
+	return vals
+}
+
+func removeNthLastNode(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	follower, leader := head, head
+	for i := 0; i < n; i++ {
+		leader = leader.Next
+		// If n is the length of the linked list, then fast will be nil.
+		// In this case, we want to remove the head of the linked list.
+		if leader == nil {
+			return head.Next
+		}
+	}
+
+	for leader != nil && leader.Next != nil {
+		follower = follower.Next
+		leader = leader.Next
+	}
+
+	if follower.Next == nil {
+		return nil
+	}
+
+	follower.Next = follower.Next.Next
+	return head
+}
--- two_pointers/twopointers_test.go
+++ two_pointers/twopointers_test.go
@@ -44,3 +44,61 @@ func TestIsPalindrome(t *testing.T) {
 		})
 	}
 }
+
+func TestRemoveNthLastNode(t *testing.T) {
+	testCases := []struct {
+		listInfo func() *ListNode
+		n        int
+		want     *LinkedList
+	}{
+		{
+			listInfo: func() *ListNode {
+				ll := InitLinkedList([]int{1, 2, 3, 4, 5})
+				return ll.Head
+			},
+			n:    2,
+			want: InitLinkedList([]int{1, 2, 3, 5}),
+		},
+		{
+			listInfo: func() *ListNode {
+				ll := InitLinkedList([]int{1})
+				return ll.Head
+			},
+			n:    1,
+			want: nil,
+		},
+		{
+			listInfo: func() *ListNode {
+				ll := InitLinkedList([]int{1, 2})
+				return ll.Head
+			},
+			n:    1,
+			want: InitLinkedList([]int{1}),
+		},
+		{
+			listInfo: func() *ListNode {
+				ll := InitLinkedList([]int{1, 2})
+				return ll.Head
+			},
+			n:    2,
+			want: InitLinkedList([]int{2}),
+		},
+		{
+			listInfo: func() *ListNode {
+				return nil
+			},
+			n:    1,
+			want: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		head := tc.listInfo()
+		got := removeNthLastNode(head, tc.n)
+		if tc.want == nil {
+			assert.Nil(t, got)
+			continue
+		}
+		assert.Equal(t, tc.want.Display(), ConstructLinkedListFromNode(got).Display())
+	}
+}
