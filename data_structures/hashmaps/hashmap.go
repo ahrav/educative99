277168f22619a31d52d066d233940834fb98b3d2
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-03-31 13:24:48 -0700

    hashmap stuff.

---
+++ data_structures/hashmaps/hashmap.go
@@ -0,0 +1,52 @@
+package hashmaps
+
+type RequestLogger struct {
+	m     map[string]int
+	limit int
+}
+
+func (l *RequestLogger) requestLoggerInit(timeLimit int) {
+	l.m = make(map[string]int)
+	l.limit = timeLimit
+}
+
+func (l *RequestLogger) messageRequestDecision(timestamp int, request string) bool {
+	val, ok := l.m[request]
+	if !ok {
+		l.m[request] = timestamp
+		return true
+	}
+
+	if timestamp-val >= l.limit {
+		l.m[request] = timestamp
+		return true
+	}
+
+	return false
+}
+
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	stack := make([]int, 0, len(nums2))
+	m := make(map[int]int)
+
+	for _, current := range nums2 {
+		for len(stack) > 0 && current > stack[len(stack)-1] {
+			m[stack[len(stack)-1]] = current
+			stack = stack[:len(stack)-1]
+		}
+
+		stack = append(stack, current)
+	}
+
+	ans := make([]int, len(nums1))
+
+	for i, num := range nums1 {
+		if val, ok := m[num]; ok {
+			ans[i] = val
+			continue
+		}
+		ans[i] = -1
+	}
+
+	return ans
+}
---
+++ data_structures/hashmaps/hashmap_test.go
@@ -0,0 +1,15 @@
+package hashmaps
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNextGreaterElement(t *testing.T) {
+	input1 := []int{137, 59, 92, 122, 52, 131, 79, 236}
+	input2 := []int{137, 59, 92, 122, 52, 131, 79, 236}
+	want := []int{236, 92, 122, 131, 131, 236, 236, -1}
+	got := nextGreaterElement(input1, input2)
+	assert.Equal(t, want, got)
+}
