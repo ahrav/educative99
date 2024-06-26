4174eea43e7bd91c4893a517836b46fa24128391
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-06-26 16:02:03 -0700

    merge sorted slices.

--- two_pointers/twopointers_test.go
+++ two_pointers/twopointers_test.go
@@ -314,3 +314,19 @@ func TestBinarySearchRotated(t *testing.T) {
 		assert.Equal(t, tc.want, got)
 	}
 }
+
+func BenchmarkMergeSortedSlices(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mergeSortedSlices([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+	}
+}
+
+func TestMergeSortedSlices(t *testing.T) {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+
+	mergeSortedSlices(nums1, m, nums2, n)
+	assert.Equal(t, []int{1, 2, 2, 3, 5, 6}, nums1)
+}
